af9b62ff35c354b1cb8a847ee566ee0637d0021c
phmiranda <pehhagah.1607@gmail.com>
2019-10-07 17:17:58 -0300

    [add] - inclusão das configurações padrão para a base de dados local

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -20,16 +20,16 @@
 
 // ** Configurações do MySQL - Você pode pegar estas informações com o serviço de hospedagem ** //
 /** O nome do banco de dados do WordPress */
-define('DB_NAME', 'nome_do_banco_de_dados_aqui');
+define('DB_NAME', 'db_website');
 
 /** Usuário do banco de dados MySQL */
-define('DB_USER', 'nome_de_usuario_aqui');
+define('DB_USER', 'root');
 
 /** Senha do banco de dados MySQL */
-define('DB_PASSWORD', 'senha_aqui');
+define('DB_PASSWORD', 'root');
 
 /** Nome do host do MySQL */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost.dsv.com.br');
 
 /** Charset do banco de dados a ser usado na criação das tabelas. */
 define('DB_CHARSET', 'utf8');
